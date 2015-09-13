0851ec37bc000b5570da7bfaef4c0d49fd131aa3
clach04 <Chris.Clark@actian.com>
2015-09-13 11:18:49 -0700

    Experiment with EffectLayer
    Import C files of rev dbef3f9dd0d62e6eb3ad3d65267cffff07f56325 from https://github.com/ygalanter/EffectLayer

--- src/main.c
+++ src/main.c
@@ -14,11 +14,44 @@
 
 #include "watchface.h"
 
+#include "effect_layer.h"
+
+
 static BitmapLayer *s_background_layer=NULL;  /* NOTE s_bg_image is NOT defined */
 
 static uint32_t s_bg_image=RESOURCE_ID_IMAGE_RENEGADE;  // or RESOURCE_ID_IMAGE_PARAGON
 static GBitmap     *s_background_bitmap_renegade=NULL;
 static GBitmap     *s_background_bitmap_paragon=NULL;
+
+static EffectLayer* effect_layer=NULL;
+static EffectOffset shadow;
+
+void setup_effects(Window *window)
+{
+    //effect_layer = effect_layer_create(GRect(0, 0, 144, 168)); // will cover entire screen
+    effect_layer = effect_layer_create(CLOCK_POS);
+
+    shadow.orig_color = time_color;
+    shadow.offset_color = COLOR_FALLBACK(GColorYellow, background_color); // FIXME/TODO
+    shadow.offset_y = 2;
+    shadow.offset_x = 2;
+
+    //effect_layer_add_effect(effect_layer, effect_shadow, &shadow);
+    effect_layer_add_effect(effect_layer, effect_outline, &shadow);
+    //effect_layer_add_effect(effect_layer, effect_invert, NULL);
+    //effect_layer_add_effect(effect_layer, effect_invert_bw_only, NULL);
+    //effect_layer_add_effect(effect_layer, effect_rotate_90_degrees, (void *)true); // rotates 90 degrees counterclockwise
+
+    layer_add_child(window_get_root_layer(window), effect_layer_get_layer(effect_layer));
+}
+
+void cleanup_effects()
+{
+    if (effect_layer)
+    {
+        effect_layer_destroy(effect_layer);
+    }
+}
 
 void custom_main_window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
@@ -46,12 +79,20 @@ void custom_main_window_load(Window *window) {
     window_set_background_color(main_window, background_color);
 
     layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
-    
+
     /* End of custom code, call generic code */
     main_window_load(window);
+
+    /* Custom code */
+    setup_effects(window); // move me?
+    // do another redreaw :-(
+    update_time();
+
 }
 
 void custom_main_window_unload(Window *window) {
+    cleanup_effects();
+
     /* Call generic code */
     main_window_unload(window);
 
